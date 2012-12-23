73d9a09539f99b41246ca42e8b5919cf874f43c5
Ivan Tolkach <ivan@tolkach.com>
2012-12-24 00:22:12 +0200

    logout

--- zo/zo/urls.py
+++ zo/zo/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import logout
 
 admin.autodiscover()
 from django.conf import settings
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^register/$', 'zo.auth.views.register', name='register'),
+    url(r'^logout/$', logout, {"next_page": "/"}, name='logout'),
 )
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
